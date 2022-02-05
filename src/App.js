bc6f00ba2405b8e7b017d09570272fffb7937ec6
VeronicaGuerrero <veri_bellas96@hotmail.com>
2022-02-05 17:52:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import Clock from './Clock';
 import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2022',
+            newDeadline: ''
+        }
+    }
+
+    changeDeadline() {
+        this.setState({ deadline: this.state.newDeadline });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div class="jumbotron">
+                    <div className="App-title">
+                        Countdown to {this.state.deadline}
+                    </div>
+                    <Clock
+                        deadline={this.state.deadline}
+                    />
+                    <Form inline>
+                        <FormControl
+                            className="Deadline-input"
+                            placeholder='new date'
+                            onChange={event => this.setState({ newDeadline: event.target.value })}
+                        />
+                        <Button onClick={() => this.changeDeadline()}>
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
